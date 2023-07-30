443f80e6d07d31f5ad77d0c72bcd09c10b34dc06
alexrnguyen <alexander.nguyen@shaw.ca>
2023-07-29 21:00:45 -0600

    Add modal overlay

--- app.js
+++ app.js
@@ -9,14 +9,26 @@ class Book {
     }
 }
 
+function triggerModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.remove('hidden');
+
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.remove('hidden');
+}
+
 function addBookToLibrary() {
     const bookGrid = document.querySelector('.book-grid');
 
     // Temporary code
     const newBook = new Book('Test', 'Alex Nguyen', 0, true);
     library.push(newBook);
-    bookGrid.textContent = library;
+
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+    bookCard.textContent(newBook.title); // Testing purposes
+    bookGrid.appendChild(bookCard);
 }
 
 const addButton = document.querySelector('.add-button');
-addButton.addEventListener('click', addBookToLibrary)
+addButton.addEventListener('click', triggerModal);
